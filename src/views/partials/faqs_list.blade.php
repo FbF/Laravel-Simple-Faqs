e600d0ac77e44d81088ec657ffdbf8084da748ba
Dan Claydon <dan.claydon@fivebyfiveuk.com>
2013-12-16 18:05:36 +0000

    Markup changes

--- src/views/partials/faqs_list.blade.php
+++ src/views/partials/faqs_list.blade.php
@@ -1,18 +1,20 @@
 @if (!$faqs->isEmpty())
+    <div class="faq">
 	@if (Config::get('laravel-simple-faqs::show_questions_at_top'))
-		<ul class="faq-questions">
+		<ul class="faq--nav">
 			@foreach ($faqs as $faq)
 			<li><a href="#{{ $faq->slug }}">{{ $faq->question }}</a></li>
 			@endforeach
 		</ul>
 	@endif
 	@foreach ($faqs as $faq)
-		<div>
+		<div class="faq--item">
 			<h2 id="{{ $faq->slug }}">{{ $faq->question }}</h2>
 			{{ $faq->answer }}
-			<p class="top"><a href="#top">Back to top</a></p>
+			<p class="faq--top-link"><a href="#top">Back to top</a></p>
 		</div>
 	@endforeach
+	</div>
 @else
 	<p>There aren't any FAQs at the moment.</p>
 @endif
